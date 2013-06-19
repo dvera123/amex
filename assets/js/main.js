42ae1dd683679db5ea7b3032f9b20d92b4230b59
Erick Espinoza <egea19984@gmail.com>
2013-06-19 15:51:02 -0600

    Update modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-      $("#modals").load("modals/modals.html"); 
+      $("#modals").load("modals/modals.html");
+      //active the modal view 
       $(".btn").click(function(){
+      	//enables the tooltip for the modal
       	$('.atooltip').mouseover(function(){
       		var id = $(this).attr("id");
       		$("."+id).show();
@@ -8,9 +10,35 @@ $(document).ready(function(){
       		var id = $(this).attr("id");
       		$("."+id).hide();
       	});
-
+      	//enables the sort of the tables
       	$("#table-pay-overtime").tablesorter();
-
+      	//enables first three elements of the table
+      	$("#table-pay-overtime tbody tr").each(function(i){
+      		if(i>2){
+	      		$(this).find("td").each(function(i){
+	      			if(i==0){
+	      				$(this).find("input").attr("disabled","disabled");
+	      			}
+	      		});
+	      	}
+      	});
+      	//click for continue button
+      	$("#continue").click(function(){
+      		var n = $("input:checked").length;
+      		if(n > 0){
+      			$("#first-body").hide();
+      			$("#second-body").show();
+      		}
+      	});
+      	$("#submit").click(function(){
+      		$("#second-body").hide();
+      		$("#third-body").show();
+      	});
+      	$("#back").click(function(){
+      		$("#first-body").show();
+      		$("#second-body").hide();
+      	});
+      	//enables the drawer on the footer
       	$(".show-element").click(function(){
       		var id = $(this).attr("id");
       		$("."+id).show();
